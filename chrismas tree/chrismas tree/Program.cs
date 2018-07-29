8961f3dc879d737e3d8a1ef6bd6959da41e246bf
mag12242002 <mag12242002@gmail.com>
2018-07-29 22:03:13 +0800

    first upload

---
+++ chrismas tree/chrismas tree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chrismas_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heigh=0;
+            while(heigh++<5)
+            {
+                int a=0;
+                while(a++<5-heigh)
+                {
+                    Console.Write(" ");
+                }
+                int b = 0;
+                while (b++ < 2 * heigh - 1)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
+    
---
+++ guess number/guess number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guess_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Rnd = new Random();
+            int num = Convert.ToInt32(Rnd.Next(1, 100));
+            int guess = 1000;
+
+
+            while (guess != num)
+            {
+                Console.Write("請輸入數字:");
+                guess =Convert.ToInt32(Console.ReadLine());
+
+                if (guess > num)
+                {
+                    Console.WriteLine("猜的數字太大囉!");
+                    Console.WriteLine("");
+                }
+
+                if (guess < num)
+                {
+                    Console.WriteLine("猜的數字太小囉!");
+                    Console.WriteLine("");
+                }
+
+                if (guess == num)
+                {
+                    Console.WriteLine("恭喜你猜對囉!");
+                    Console.WriteLine("");
+                }
+            }
+            
+
+            
+        }
+    }
+}
